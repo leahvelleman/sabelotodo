a2c01754b45a30e1408b5ba6d60eea2599d0e149
leahvelleman <leahvelleman@gmail.com>
2020-03-21 14:10:47 -0400

    typo fix in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_UI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from models import Item
