be251375f14c8d3300ba8e4ea5950043f4e3f9fc
leahvelleman <leahvelleman@gmail.com>
2020-03-22 12:11:41 -0400

    Add line to silence warning message

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Suppress a warning message
 db = SQLAlchemy(app)
 
 from models import Item
