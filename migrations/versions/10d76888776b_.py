5277479652fb9d217b86f343f245e4bc3f01fb4a
leahvelleman <leahvelleman@gmail.com>
2020-03-22 12:14:24 -0400

    Update to migration files

--- migrations/versions/10d76888776b_.py
+++ migrations/versions/10d76888776b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 265b4e48e5d0
+Revision ID: 10d76888776b
 Revises: 
-Create Date: 2020-03-21 14:25:51.517423
+Create Date: 2020-03-22 12:01:39.975825
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '265b4e48e5d0'
+revision = '10d76888776b'
 down_revision = None
 branch_labels = None
 depends_on = None
