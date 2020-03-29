f2a93191394bd75c068296a6707adc09db54060f
Rachel Stevens <rachel.stevens@reversinglabs.com>
2020-03-29 14:18:44 -0400

    Test fixture for creating database

--- sabelotodo/__init__.py
+++ sabelotodo/__init__.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(config_object='config.Config'):
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app.config.from_object(config_object)
     db.init_app(app)
 
     with app.app_context():
---
+++ sabelotodo/tests/test_hello.py
@@ -0,0 +1,68 @@
+import json
+import os
+import pytest
+import sqlalchemy as sa
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from pytest_postgresql.factories import (DatabaseJanitor,
+                                         drop_postgresql_database)
+from sabelotodo.models import Item, db
+
+
+# Retrieve a database connection string from the shell environment
+try:
+    DB_CONN = os.environ['TEST_DATABASE_URL']
+except KeyError:
+    raise KeyError('TEST_DATABASE_URL not found. You must export a database ' +
+                   'connection string to the environmental variable ' +
+                   'TEST_DATABASE_URL in order to run tests.')
+else:
+    DB_OPTS = sa.engine.url.make_url(DB_CONN).translate_connect_args()
+
+pytest_plugins = ['pytest-flask-sqlalchemy']
+
+@pytest.fixture(scope='session')
+def database(request):
+    '''
+    Create a Postgres database for the tests, and drop it when the tests are done.
+    '''
+    pg_host = "localhost"
+    pg_port = "5432"
+    pg_user = "postgres"
+    pg_db = "sabelotodo_test"
+
+    janitor = DatabaseJanitor(pg_user, pg_host, pg_port, pg_db, 11.7)
+    janitor.init()
+
+    @request.addfinalizer
+    def drop_database():
+        janitor.drop()
+
+
+@pytest.fixture(scope='session')
+def app(database):
+    '''
+    Create a Flask app context for the tests.
+    '''
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONN
+
+    return app
+
+
+@pytest.fixture(scope='session')
+def _db(app):
+    '''
+    Provide the transactional fixtures with access to the database via a Flask-SQLAlchemy
+    database connection.
+    '''
+    db = SQLAlchemy(app=app)
+
+    return db
+
+
+def test_create_item(db_session):
+    i = Item(name='TestItem', order=0, done=True)
+    db.session.add(i)
+    db.session.commit()
