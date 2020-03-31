063e40b4f86cae89f8eeb9f82bd98b01e22185d2
leahvelleman <leahvelleman@gmail.com>
2020-03-31 19:24:57 -0400

    Better docstrings for test functions

--- sabelotodo/tests/test_routes.py
+++ sabelotodo/tests/test_routes.py
@@ -3,6 +3,7 @@ from dataclasses import asdict
 
 import json
 
+
 def test_create_item(_db):
     """ Verify that database works"""
     name = 'TestItem'
@@ -15,12 +16,15 @@ def test_create_item(_db):
     item = Item.query.filter_by(name=name).first()
     assert i == item
 
+
 def test_item_route_with_empty_db(test_client, _db):
     rv = test_client.get('/item')
     assert json.loads(rv.data) == []
 
+
 def test_item_route_with_multiple_items(test_client, _db):
-    """ Verify that the /item route retrieves all items added """
+    """ The /item route retrieves all items added. """
+
     i = Item(name="a", order=0, done=False)
     j = Item(name="b", order=2, done=True)
     k = Item(name="c", order=1, done=False)
@@ -30,12 +34,13 @@ def test_item_route_with_multiple_items(test_client, _db):
     _db.session.add(k)
     _db.session.commit()
 
-    rv = test_client.get('/item') 
+    rv = test_client.get('/item')
     assert json.loads(rv.data) == [asdict(i) for i in Item.query.all()]
 
+
 def test_itemid_route_with_valid_id(test_client, _db):
-    """ Verify that the /item/<id> route retrieves just the item with the ID in
-    question"""
+    """ The /item/<id> route retrieves just the item with the specified ID. """
+
     i = Item(name="a", order=0, done=False)
     j = Item(name="b", order=2, done=True)
     k = Item(name="c", order=1, done=False)
@@ -49,7 +54,11 @@ def test_itemid_route_with_valid_id(test_client, _db):
     rv = test_client.get('/item/%s' % this_id)
     assert json.loads(rv.data) == asdict(j)
 
+
 def test_itemid_route_failures(test_client, _db):
+    """ The /item/<id> route fails with a 404 for IDs that don't convert to
+    integers or that don't correspond to an item in the database. """
+
     i = Item(name="a", order=0, done=False)
     j = Item(name="b", order=2, done=True)
     k = Item(name="c", order=1, done=False)
@@ -62,7 +71,13 @@ def test_itemid_route_failures(test_client, _db):
     rv = test_client.get('/item/999')
     assert rv.status_code == 404
 
+    rv = test_client.get('/item/0.5')
+    assert rv.status_code == 404
+
     rv = test_client.get('/item/-1')
+    assert rv.status_code == 404
+
+    rv = test_client.get('/item/1&garbage')
     assert rv.status_code == 404
 
     rv = test_client.get('/item/')
@@ -70,4 +85,3 @@ def test_itemid_route_failures(test_client, _db):
 
     rv = test_client.get('/item/spork')
     assert rv.status_code == 404
-
