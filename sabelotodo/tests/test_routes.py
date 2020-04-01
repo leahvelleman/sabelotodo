43e5a21222a4ecd84330827fd3ca6cd5f3bff43a
leahvelleman <leahvelleman@gmail.com>
2020-04-01 14:44:30 -0400

    Refactor tests to use parametrize and reduce other boilerplate.

--- sabelotodo/tests/test_routes.py
+++ sabelotodo/tests/test_routes.py
@@ -1,7 +1,9 @@
-from sabelotodo.models import Item
+import pytest, json, sys
 from dataclasses import asdict
+from itertools import product
 
-import json
+from sabelotodo.models import Item
+
 
 
 def test_create_item(_db):
@@ -25,63 +27,67 @@ def test_item_route_with_empty_db(test_client, _db):
 def test_item_route_with_multiple_items(test_client, _db):
     """ The /item route retrieves all items added. """
 
-    i = Item(name="a", order=0, done=False)
-    j = Item(name="b", order=2, done=True)
-    k = Item(name="c", order=1, done=False)
+    items = [Item(name="a", order=0, done=False),
+             Item(name="b", order=2, done=True),
+             Item(name="c", order=1, done=False)]
 
-    _db.session.add(i)
-    _db.session.add(j)
-    _db.session.add(k)
+    _db.session.add_all(items)
     _db.session.commit()
 
     return_value = test_client.get('/item')
     assert json.loads(return_value.data) == [asdict(i) for i in Item.query.all()]
 
 
-def test_itemid_route_with_valid_id(test_client, _db):
+@pytest.mark.parametrize("idx", [0, 1, 2])
+def test_get_itemid_route_with_valid_id(test_client, _db, idx):
     """ The /item/<id> route retrieves just the item with the specified ID. """
 
-    i = Item(name="a", order=0, done=False)
-    j = Item(name="b", order=2, done=True)
-    k = Item(name="c", order=1, done=False)
+    items = [Item(name="a", order=0, done=False),
+             Item(name="b", order=2, done=True),
+             Item(name="c", order=1, done=False)]
 
-    _db.session.add(i)
-    _db.session.add(j)
-    _db.session.add(k)
+    _db.session.add_all(items)
     _db.session.commit()
 
-    this_id = j.id
-    return_value = test_client.get('/item/%s' % this_id)
-    assert json.loads(return_value.data) == asdict(j)
+    selection = items[idx]
+    return_value = test_client.get('/item/%s' % selection.id)
+    assert json.loads(return_value.data) == asdict(selection)
 
 
-def test_itemid_route_failures(test_client, _db):
+@pytest.mark.parametrize("idx", [0, 1, 2])
+def test_delete_itemid_route_with_valid_id(test_client, _db, idx):
+    """ A DELETE request to /item/<id> deletes the item with the
+    specified ID. """
+
+    items = [Item(name="a", order=0, done=False),
+             Item(name="b", order=2, done=True),
+             Item(name="c", order=1, done=False)]
+
+    _db.session.add_all(items)
+    _db.session.commit()
+
+    selection = items[idx]
+    remainder = items[:idx] + items[idx+1:]
+    return_value = test_client.delete('/item/%s' % selection.id)
+    assert return_value.status_code == 200
+    assert sorted(Item.query.all()) == sorted(remainder)
+
+
+@pytest.mark.parametrize(
+        "itemid, method", 
+        product([sys.maxsize, "0.5", "-1", "1&garbage", "", "spork"],
+                ["GET", "DELETE"]))
+def test_itemid_route_failures(test_client, _db, itemid, method):
     """ The /item/<id> route fails with a 404 for IDs that don't convert to
-    integers or that don't correspond to an item in the database. """
+    integers or that don't correspond to an item in the database, and
+    does so for any of the methods we support. """
 
-    i = Item(name="a", order=0, done=False)
-    j = Item(name="b", order=2, done=True)
-    k = Item(name="c", order=1, done=False)
+    items = [Item(name="a", order=0, done=False),
+             Item(name="b", order=2, done=True),
+             Item(name="c", order=1, done=False)]
 
-    _db.session.add(i)
-    _db.session.add(j)
-    _db.session.add(k)
+    _db.session.add_all(items)
     _db.session.commit()
 
-    return_value = test_client.get('/item/999')
-    assert return_value.status_code == 404
-
-    return_value = test_client.get('/item/0.5')
-    assert return_value.status_code == 404
-
-    return_value = test_client.get('/item/-1')
-    assert return_value.status_code == 404
-
-    return_value = test_client.get('/item/1&garbage')
-    assert return_value.status_code == 404
-
-    return_value = test_client.get('/item/')
-    assert return_value.status_code == 404
-
-    return_value = test_client.get('/item/spork')
+    return_value = test_client.open(method=method, path='/item/%s' % itemid)
     assert return_value.status_code == 404
