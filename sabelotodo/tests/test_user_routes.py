44d6891f5f816746c5f185dca5a790a870cd9c41
leahvelleman <leahvelleman@gmail.com>
2020-04-11 09:55:49 -0400

    Add test for GET user route. Still requires manually clearing the database fixture. Don't know why.

--- sabelotodo/tests/test_user_routes.py
+++ sabelotodo/tests/test_user_routes.py
@@ -5,6 +5,7 @@ from .testdata import VALID_USER_DATA, INVALID_USER_DATA
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
+
 def test_user_route_with_multiple_users(test_client, _db):
     """ The /user route retrieves all users added. """
 
@@ -17,3 +18,23 @@ def test_user_route_with_multiple_users(test_client, _db):
 
     assert return_value.status_code == 200, "The request succeeds."
     assert returned_users == User.query.all(), "Returned users match database."
+
+
+@pytest.mark.parametrize("idx", range(len(VALID_USER_DATA)))
+def test_get_userid_route_with_valid_id(test_client, _db, idx):
+    """ The /user/<id> route retrieves just the user with the specified ID. """
+
+    users = users_schema.load(VALID_USER_DATA)
+    _db.session.add_all(users)
+    _db.session.commit()
+
+    selection = users[idx]
+    return_value = test_client.get('/user/%s' % selection.id)
+    returned_user = user_schema.loads(return_value.data)
+
+    assert return_value.status_code == 200, "The request succeeds."
+    assert returned_user == selection, "We get back the user we expect."
+
+    _db.session.rollback()  # TODO: This shouldn't be necessary, and for
+    # the tests in test_routes.py it *isn't* necessary. Figure out why.
+    # LV
